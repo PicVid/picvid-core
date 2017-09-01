59085e5c1b8fb9d9912d79ad3c0f421b7af159f6
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-01 20:01:01 +0200

    added the default controller as example.

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Namespace for all controller of PicVid.
+ */
+namespace PicVid\Controller;
+
+/**
+ * Class IndexController
+ *
+ * @author Sebastian Brosch <contact@sebastianbrosch.de>
+ * @license GNU General Public License, version 3
+ * @package PicVid\Controller
+ */
+class IndexController
+{
+    /**
+     * The default method / action of the controller.
+     */
+    public function index()
+    {
+        echo 'Hello World';
+    }
+}
