12d0360dfc90bc5ffb28d5b85089f5a5398c9240
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-12 23:50:41 +0200

    added register controller.

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Namespace for all Controller of PicVid.
+ */
+namespace PicVid\Controller;
+
+use PicVid\Core\CitoEngine;
+use PicVid\Core\View;
+
+/**
+ * Class RegisterController
+ *
+ * @author Sebastian Brosch <contact@sebastianbrosch.de>
+ * @license GNU General Public License, version 3
+ * @package PicVid\Controller
+ */
+class RegisterController
+{
+    /**
+     * The default method / action of the Controller.
+     */
+    public function index()
+    {
+        //set the values for the template tags / placeholders on CitoEngine.
+        $cito = CitoEngine::getInstance();
+        $cito->setValue('BODY_ID', 'register-view');
+        $cito->setValue('LOGO_URL', URL.'/resource/template/img/picvid-logo.png');
+
+        //load the view.
+        $view = new View('Register');
+        $view->load();
+    }
+}
