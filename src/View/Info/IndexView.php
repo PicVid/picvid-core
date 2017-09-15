e6b0259a1ee9216f52fe42a2c98379a575957958
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-15 22:55:25 +0200

    added the info controller and view.

---
+++ src/View/Info/IndexView.php
@@ -0,0 +1,62 @@
+<div class="bg-overlay"></div>
+<div class="container-fluid d-flex flex-column">
+    <div class="d-flex flex-row justify-content-start align-items-center p-5 mb-5">
+        <a href="<?= URL ?>">
+            <img class="logo" height="60" src="{{LOGO_URL}}"/>
+        </a>
+    </div>
+    <div class="row justify-content-center px-xl-5">
+        <div class="col-xl-3 col-lg-5 col-md-5 col-sm-11">
+            <h2>Willkommen bei PicVid</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+        </div>
+        <div class="col-xl-3 col col-lg-5 col-md-5 col-sm-11">
+            <h2>Willkommen bei PicVid</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+        </div>
+        <div class="col-xl-3 col-lg-5 col-md-5 col-sm-11 mt-xl-0 mt-lg-4">
+            <h2>Willkommen bei PicVid</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+        </div>
+        <div class="col-xl-3 col-lg-5 col-md-5 col-sm-11 mt-xl-0 mt-lg-4">
+            <h2>Willkommen bei PicVid</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
+                facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
+                vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
+                mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
+        </div>
+    </div>
+</div>
