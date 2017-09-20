60c4da2297a4ff04f155d96d961f66cde5e0bfa6
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-20 19:51:15 +0200

    not important changes :)

--- src/Core/Service/SessionService.php
+++ src/Core/Service/SessionService.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Namespace for all services of the core of PicVid.
+ * Namespace for all Services of the Core of PicVid.
  */
 namespace PicVid\Core\Service;
 
@@ -18,22 +18,23 @@ use PicVid\Domain\Entity\User;
 class SessionService
 {
     /**
-     * Method to create a session.
-     * @param User $user The User entity for which the Session is to be created.
+     * Method to create a Session.
+     * @param User $user The User Entity for which the Session is to be created.
      * @return void
      */
     public function create(User $user)
     {
-        //create the session.
+        //create the Session.
         $session = new Session();
         $session->create(Database::getInstance()->getConnection());
 
-        //set the information to the session.
+        //set the information to the Session.
         $_SESSION['user_username'] = $user->username;
+        $_SESSION['user_id'] = $user->id;
     }
 
     /**
-     * Method to delete a session.
+     * Method to delete a Session.
      * @return void
      */
     public function delete()
