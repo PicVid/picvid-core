b9eff9509a0ef4dbcb8c1380d202d19ebf41856d
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-20 19:59:40 +0200

    replaced svg image with font awesome icon. some design changes.

--- src/View/Index/IndexView.php
+++ src/View/Index/IndexView.php
@@ -1,21 +1,19 @@
 <div class="bg-overlay"></div>
-<a class="credits" href="https://unsplash.com/@abrkett?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Adam Birkett">
-    <span>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><title></title><path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path></svg>
-    </span>
+<a class="credits" href="https://unsplash.com/@abrkett?utm_medium=referral&utm_campaign=photographer-credit&utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Adam Birkett">
+    <span><i class="fa fa-camera" aria-hidden="true"></i></span>
     <span>Adam Birkett</span>
 </a>
 <div class="container-fluid d-flex flex-column justify-content-center">
     <div class="row align-self-center">
         <div class="col text-center">
-            <img class="logo" height="85" src="{{LOGO_URL}}"/>
+            <img class="logo" height="90" src="{{LOGO_URL}}"/>
         </div>
     </div>
     <div class="row align-self-center">
         <div class="col text-center">
-            <a class="btn btn-success" href="<?= URL ?>register">Register</a>
-            <a class="btn btn-success" href="<?= URL ?>login">Login</a>
-            <a class="btn btn-primary" href="<?= URL ?>info">Info</a>
+            <a class="btn btn-success mr-2" href="<?= URL ?>register">Register</a>
+            <a class="btn btn-success" href="<?= URL ?>auth">Login</a>
+            <a class="btn btn-primary ml-2" href="<?= URL ?>info">Info</a>
         </div>
     </div>
 </div>
