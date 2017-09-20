8e43acefd748c555c0e8025d5cc1e451b74b8b7c
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-20 19:58:24 +0200

    added possibility to login.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -10,16 +10,16 @@ use PicVid\Core\View;
 use PicVid\Domain\Entity\User;
 
 /**
- * Class LoginController
+ * Class AuthController
  *
  * @author Sebastian Brosch <coding@sebastianbrosch.de>
  * @license GNU General Public License, version 3
  * @package PicVid\Controller
  */
-class LoginController extends Controller
+class AuthController extends Controller
 {
     /**
-     * The default method / action of the Controller.
+     * The default method / action of the controller.
      */
     public function index()
     {
@@ -30,20 +30,37 @@ class LoginController extends Controller
         $cito->setValue('LOGO_URL', URL.'/resource/template/img/picvid-logo.png');
 
         //load the view.
-        $view = new View('Login');
+        $view = new View('Auth');
         $view->load();
     }
 
     /**
-     * The login method / action of the Controller.
+     * The login method / action of the controller.
      */
     public function login()
     {
-        //load the user from register form.
+        //load the User Entity from login form.
         $user = new User();
         $user->loadFromPOST('login_');
 
-        //login the User Entity.
-        (new AuthenticationService())->login($user);
+        //login the User Entity and redirect to the profile page.
+        if ((new AuthenticationService())->login($user)) {
+            $this->redirect(URL.'profile');
+        }
+    }
+
+    /**
+     * The logout method / action of the controller.
+     */
+    public function logout()
+    {
+        //we need a session to logout.
+        $this->needSession();
+
+        //logout the current User Entity / destroy the current session.
+        (new AuthenticationService())->logout();
+
+        //redirect to the index view.
+        $this->redirect(URL);
     }
 }
