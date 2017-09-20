1c6fa98efc32944ff2005d625a13dc8e2638ee3d
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-20 20:20:33 +0200

    added design for profile page.

--- resource/template/header.php
+++ resource/template/header.php
@@ -6,6 +6,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="<?= URL ?>resource/template/picvid.css"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
     <link rel="apple-touch-icon" sizes="57x57" href="<?= URL ?>resource/template/img/icons/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?= URL ?>resource/template/img/icons/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="<?= URL ?>resource/template/img/icons/apple-icon-72x72.png">
