914e6e1c55732382af7d5b2523ab93011a2c30d9
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-23 11:06:06 +0200

    now we can find User Entities by email.

--- src/Domain/Repository/UserRepository.php
+++ src/Domain/Repository/UserRepository.php
@@ -30,12 +30,35 @@ class UserRepository extends Repository
      */
     public function findAll() : array
     {
-        //check if a UserMapper is available.
+        //check if an UserMapper is available.
         if (!($this->dataMapper instanceof UserMapper)) {
             return [];
         } else {
             return $this->findAllEntities(get_class($this->dataMapper));
         }
+    }
+
+    /**
+     * Method to find and get User Entities by email.
+     * @param string $email The email to find and get the User Entities.
+     * @return array An array with all found User Entities.
+     */
+    public function findByEmail(string $email) : array
+    {
+        //check if an UserMapper is available.
+        if (!($this->dataMapper instanceof UserMapper)) {
+            return [];
+        }
+
+        //check if the parameter is an array.
+        if (is_array($email)) {
+            $condition = "email IN ('".implode("', '", $email)."')";
+        } else {
+            $condition = "email = '".$email."'";
+        }
+
+        //return the result of the UserMapper.
+        return $this->dataMapper->find($condition);
     }
 
     /**
@@ -45,7 +68,7 @@ class UserRepository extends Repository
      */
     public function findByID(int $id) : array
     {
-        //check if a UserMapper is available.
+        //check if an UserMapper is available.
         if (!($this->dataMapper instanceof UserMapper)) {
             return [];
         } else {
@@ -60,7 +83,7 @@ class UserRepository extends Repository
      */
     public function findByUsername(string $username) : array
     {
-        //check if a UserMapper is available.
+        //check if an UserMapper is available.
         if (!($this->dataMapper instanceof UserMapper)) {
             return [];
         }
