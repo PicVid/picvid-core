9e1c1aa6a69d3d0995241517594e917609fd3c11
sebastianbrosch <contact@sebastianbrosch.de>
2017-09-23 11:07:43 +0200

    added Specifications for unique email and username.

---
+++ src/Domain/Specification/User/IsUniqueUsername.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Namespace for all Specifications of the User Entities.
+ */
+namespace PicVid\Domain\Specification\User;
+
+use PicVid\Domain\Entity\IEntity;
+use PicVid\Domain\Entity\User;
+use PicVid\Domain\Repository\UserRepository;
+use PicVid\Domain\Specification\ISpecification;
+
+/**
+ * Class IsUniqueUsername
+ *
+ * @author Sebastian Brosch <coding@sebastianbrosch.de>
+ * @license GNU General Public License, version 3
+ * @package PicVid\Domain\Specification\User
+ */
+class IsUniqueUsername implements ISpecification
+{
+    /**
+     * Method to check whether the User Entity satisfies the Specification.
+     * @param IEntity $user The User Entity which will be checked.
+     * @return bool The state if the User Entity satisfies the Specification.
+     */
+    public function isSatisfiedBy(IEntity $user): bool
+    {
+        //check if the Entity is an User Entity.
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        //return the state if an User Entity with the username already exists.
+        return (count(UserRepository::build()->findByUsername($user->username)) === 0);
+    }
+}
