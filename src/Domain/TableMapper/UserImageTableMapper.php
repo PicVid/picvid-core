99f3bc7dd7bb8a2c846b41f8adb60c06e116ae9c
sebastianbrosch <contact@sebastianbrosch.de>
2017-10-09 20:51:38 +0200

    added possibility to remove connections between User Entity and Image Entity.

--- src/Domain/TableMapper/UserImageTableMapper.php
+++ src/Domain/TableMapper/UserImageTableMapper.php
@@ -48,4 +48,69 @@ class UserImageTableMapper extends TableMapper
         $sth->bindParam(':image_id', $image->id, \PDO::PARAM_INT);
         return $sth->execute();
     }
+
+    /**
+     * Method to remove all connections between a User Entity and Image Entity.
+     * @param User $user The User Entity for which the connection should be removed.
+     * @param Image $image The Image Entity for which the connection should be removed.
+     * @return bool The state whether the connection between User Entity and Image Entity could be removed.
+     */
+    public function delete(User $user, Image $image) : bool
+    {
+        //check whether a ID is available on the User Entity and Image Entity.
+        if (!$user->hasID() || !$image->hasID()) {
+            return false;
+        }
+
+        //delete the connection in database and return the state.
+        $sql = 'DELETE FROM `user_image` WHERE `user_id` = :user_id AND `image_id` = :image_id;';
+        $sth = $this->pdo->prepare($sql);
+
+        //bind and execute the query and return the state.
+        $sth->bindParam(':user_id', $user->id, \PDO::PARAM_INT);
+        $sth->bindParam(':image_id', $image->id, \PDO::PARAM_INT);
+        return $sth->execute();
+    }
+
+    /**
+     * Method to remove all connections of a Image Entity.
+     * @param Image $image The Image Entity for which all connections should be removed.
+     * @return bool The state whether all connections of a specific Image Entity could be removed.
+     */
+    public function deleteByImage(Image $image) : bool
+    {
+        //check whether a ID is available on the Image Entity.
+        if (!$image->hasID()) {
+            return false;
+        }
+
+        //delete the connections in database and return the state.
+        $sql = 'DELETE FROM `user_image` WHERE `image_id` = :image_id;';
+        $sth = $this->pdo->prepare($sql);
+
+        //bind the execute the query and return the state.
+        $sth->bindParam(':image_id', $image->id, \PDO::PARAM_INT);
+        return $sth->execute();
+    }
+
+    /**
+     * Method to remove all connections of a User Entity.
+     * @param User $user The User Entity for which all connections should be removed.
+     * @return bool The state whether all connections of a specific User Entity could be removed.
+     */
+    public function deleteByUser(User $user) : bool
+    {
+        //check whether a ID is available on the User Entity.
+        if (!$user->hasID()) {
+            return false;
+        }
+
+        //delete the connections in database and return the state.
+        $sql = 'DELETE FROM `user_image` WHERE `user_id` = :user_id;';
+        $sth = $this->pdo->prepare($sql);
+
+        //bind and execute the query and return the state.
+        $sth->bindParam(':user_id', $user->id, \PDO::PARAM_INT);
+        return $sth->execute();
+    }
 }
