adf0e3e3fac7c5909da9770ee9650b528c5173e3
sebastianbrosch <contact@sebastianbrosch.de>
2017-11-12 12:20:26 +0100

    removed old configuration constants.

--- config.sample.php
+++ config.sample.php
@@ -6,18 +6,6 @@ define('DB_NAME', 'database_name');
 define('DB_USER', 'username');
 define('DB_PASS', 'password');
 
-//path and url configuration.
-define('URL', 'http://example.com/');
-define('ABSPATH', __DIR__.DIRECTORY_SEPARATOR);
-define('RESPATH', ABSPATH.'resource'.DIRECTORY_SEPARATOR);
-define('SRCPATH', ABSPATH.'src'.DIRECTORY_SEPARATOR);
-define('VIEWDIR', SRCPATH.'View'.DIRECTORY_SEPARATOR);
-define('DATADIR', ABSPATH.'data'.DIRECTORY_SEPARATOR);
-define('DATAURL', URL.'data'.DIRECTORY_SEPARATOR);
-define('UPLOADDIR', DATADIR.'upload'.DIRECTORY_SEPARATOR);
-define('IMAGEDIR', DATADIR.'images'.DIRECTORY_SEPARATOR);
-define('IMAGEURL', DATAURL.'images'.DIRECTORY_SEPARATOR);
-
 //the project honeypot key.
 define('PROJECT_HONEYPOT_KEY', '');
 
