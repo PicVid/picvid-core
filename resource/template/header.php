266795c2f52158bf7cdde2a2fae405349b16e0e3
sebastianbrosch <contact@sebastianbrosch.de>
2017-11-12 12:21:48 +0100

    removed URL constant and added placeholder.

--- resource/template/header.php
+++ resource/template/header.php
@@ -5,26 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="<?= URL ?>resource/template/css/picvid.css"/>
+    <link rel="stylesheet" href="{{URL}}resource/template/css/picvid.css"/>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    <script src="<?= URL ?>/resource/template/js/picvid.js"></script>
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= URL ?>resource/template/img/icons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= URL ?>resource/template/img/icons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= URL ?>resource/template/img/icons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= URL ?>resource/template/img/icons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= URL ?>resource/template/img/icons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= URL ?>resource/template/img/icons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= URL ?>resource/template/img/icons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= URL ?>resource/template/img/icons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= URL ?>resource/template/img/icons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?= URL ?>resource/template/img/icons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= URL ?>resource/template/img/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= URL ?>resource/template/img/icons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= URL ?>resource/template/img/icons/favicon-16x16.png">
+    <script src="{{URL}}resource/template/js/picvid.js"></script>
+    <link rel="apple-touch-icon" sizes="57x57" href="{{URL}}resource/template/img/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{URL}}resource/template/img/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{URL}}resource/template/img/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{URL}}resource/template/img/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{URL}}resource/template/img/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{URL}}resource/template/img/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{URL}}resource/template/img/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{URL}}resource/template/img/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{URL}}resource/template/img/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{URL}}resource/template/img/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{URL}}resource/template/img/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{URL}}resource/template/img/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href={{URL}}resource/template/img/icons/favicon-16x16.png">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= URL ?>resource/template/img/icons/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="{{URL}}resource/template/img/icons/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <title>{{PAGE_TITLE}}</title>
     {{HEADER}}
