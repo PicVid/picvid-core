c2b86d5dd96984452e0e9188fd8de5264e6484a7
sebastianbrosch <contact@sebastianbrosch.de>
2017-11-14 20:26:08 +0100

    fixed missing " on 16x16 favicon path.

--- resource/template/header.php
+++ resource/template/header.php
@@ -22,7 +22,7 @@
     <link rel="icon" type="image/png" sizes="192x192"  href="{{URL}}resource/template/img/icons/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="{{URL}}resource/template/img/icons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="{{URL}}resource/template/img/icons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href={{URL}}resource/template/img/icons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{URL}}resource/template/img/icons/favicon-16x16.png">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{URL}}resource/template/img/icons/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
