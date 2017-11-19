cc4362d2700a0821f0a99667cf5235d6d222673c
sebastianbrosch <contact@sebastianbrosch.de>
2017-11-19 17:18:05 +0100

    fixed responsive design for register view.

--- src/View/Register/IndexView.php
+++ src/View/Register/IndexView.php
@@ -4,26 +4,19 @@
     <span>Clarisse Meyer</span>
 </a>
 <div class="container-fluid d-flex flex-column">
-    <div class="d-flex flex-row justify-content-start align-items-center p-5 mb-5">
-        <a href="{{URL}}">
-            <img class="logo" height="65" src="{{LOGO_URL}}"/>
-        </a>
+    <div class="d-flex flex-row justify-content-center justify-content-sm-center justify-content-md-start justify-content-lg-start justify-content-xl-start p-5">
+        <a href="{{URL}}"><img class="logo" height="65" src="{{LOGO_URL}}"/></a>
     </div>
-    <div class="d-flex flex-row justify-content-around align-items-center mt-5 p-5 h-50">
-        <div class="col col-4">
+    <div class="row d-flex flex-row mt-1 mt-md-3 mx-0 justify-content-center">
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-5 pr-md-5 mb-4 mb-sm-4 mb-md-4 mb-lg-0 mb-xl-0">
             <h2>Willkommen bei PicVid</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
             facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
             vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
             mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
             dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean purus felis, semper vitae dui in,
-            facilisis imperdiet erat. Morbi elit erat, interdum non nisl at, efficitur condimentum augue. Vestibulum
-            vel pellentesque magna. Mauris suscipit accumsan urna, in imperdiet nisi. Cras varius venenatis justo, vel
-            mattis neque laoreet vel. Nullam a lorem vitae nunc finibus cursus. Orci varius natoque penatibus et magnis
-            dis parturient montes, nascetur ridiculus mus. Sed ut nisl in nunc dignissim placerat.</p>
         </div>
-        <div class="col col-3">
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-5 col-xl-4 pl-md-5 mb-2">
             <form class="ajax" method="post" data-action="{{URL}}register/register">
                 <input type="hidden" name="token" value="{{token}}"/>
                 <div class="alert" role="alert"></div>
@@ -40,7 +33,7 @@
                     <input class="form-control" name="register_password" type="password" placeholder="Password"/>
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn btn-success" value="Register"/>
+                    <button type="submit" class="btn btn-success btn-sm-block"><i class="fa fa-sign-in" aria-hidden="true"></i>Register</button>
                 </div>
             </form>
         </div>
