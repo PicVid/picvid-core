7f098a480dc3c2a2a1ddaec6eb6196ec4a8bde23
sebastianbrosch <contact@sebastianbrosch.de>
2017-11-22 18:20:24 +0100

    updated setResponseCode method.

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -85,14 +85,55 @@ abstract class Controller implements IController
     }
 
     /**
-     * Method to set a response code.
-     * @param int $code The status code which will be set as response code.
+     * Method to set a HTTP response code.
+     * @param int $code The response code which will be set.
      * @return void
      */
     protected function setResponseCode(int $code)
     {
-        //TODO - create a full list of all valid codes: http://php.net/manual/en/function.http-response-code.php#107261
-        header($_SERVER["SERVER_PROTOCOL"].' '.$code.' See Other');
+        /**
+         * An overview of all available HTTP codes:
+         * 100: Continue
+         * 101: Switching Protocols
+         * 200: OK
+         * 201: Created
+         * 202: Accepted
+         * 203: Non-Authoritative Information
+         * 204: No Content
+         * 205: Reset Content
+         * 206: Partial Content
+         * 300: Multiple Choices
+         * 301: Moved Permanently
+         * 302: Moved Temporarily
+         * 303: See Other
+         * 304: Not Modified
+         * 305: Use Proxy
+         * 400: Bad Request
+         * 401: Unauthorized
+         * 402: Payment Required
+         * 403: Forbidden
+         * 404: Not Found
+         * 405: Method Not Allowed
+         * 406: Not Acceptable
+         * 407: Proxy Authentication Required
+         * 408: Request Time-out
+         * 409: Conflict
+         * 410: Gone
+         * 411: Length Required
+         * 412: Precondition Failed
+         * 413: Request Entity Too Large
+         * 414: Request-URI Too Large
+         * 415: Unsupported Media Type
+         * 500: Internal Server Error
+         * 501: Not Implemented
+         * 502: Bad Gateway
+         * 503: Service Unavailable
+         * 504: Gateway Time-out
+         * 505: HTTP Version not supported
+         */
+
+        //set the HTTP response code.
+        http_response_code($code);
     }
 
     /**
