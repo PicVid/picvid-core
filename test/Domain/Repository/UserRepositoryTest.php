f01b0b69bd1da346e94fcc1f5798fcd0420bba52
sebastianbrosch <contact@sebastianbrosch.de>
2017-12-06 21:17:52 +0100

    reviewed test cases and added some missing test cases.

--- test/Domain/Repository/UserRepositoryTest.php
+++ test/Domain/Repository/UserRepositoryTest.php
@@ -5,6 +5,7 @@
 namespace PicVid\Test\Domain\DataMapper;
 
 use PHPUnit\DbUnit\DataSet\XmlDataSet;
+use PicVid\Domain\DataMapper\ImageMapper;
 use PicVid\Domain\DataMapper\UserMapper;
 use PicVid\Domain\Entity\User;
 use PicVid\Domain\Repository\UserRepository;
@@ -34,6 +35,16 @@ class UserRepositoryTest extends DatabaseTestCase
      */
     public function testFindAll()
     {
+        //try to use another DataMapper on UserRepository then UserMapper.
+        $imageMapper = new ImageMapper($this->getConnection()->getConnection());
+        $failedUserRepository = new UserRepository($imageMapper);
+
+        //try to find a User Entity with the wrong DataMapper.
+        $arrUsers = $failedUserRepository->findAll();
+
+        //there should be no User Entity.
+        $this->assertEquals(0, count($arrUsers));
+
         //get the UserMapper and UserRepository to get the User Entities.
         $userMapper = new UserMapper($this->getConnection()->getConnection());
         $userRepository = new UserRepository($userMapper);
@@ -58,8 +69,63 @@ class UserRepositoryTest extends DatabaseTestCase
         $secondUser->username = 'ftrayes1';
 
         //The first and second User Entity should be found on database.
+        $this->assertEquals(2, count($arrUsers));
         $this->assertEquals($firstUser, $arrUsers[0]);
         $this->assertEquals($secondUser, $arrUsers[1]);
+    }
+
+    /**
+     * Method to test the UserRepository:findByEmail method.
+     * @test
+     */
+    public function testFindByEmail()
+    {
+        //try to use another DataMapper on UserRepository then UserMapper.
+        $imageMapper = new ImageMapper($this->getConnection()->getConnection());
+        $failedUserRepository = new UserRepository($imageMapper);
+
+        //try to find a User Entity with the wrong DataMapper.
+        $arrUsers = $failedUserRepository->findByEmail('tmoxon0@buzzfeed.com');
+
+        //there should be no User Entity.
+        $this->assertEquals(0, count($arrUsers));
+
+        //get the UserMapper and UserRepository to get the User Entities.
+        $userMapper = new UserMapper($this->getConnection()->getConnection());
+        $userRepository = new UserRepository($userMapper);
+
+        //get the first and second User Entity from database.
+        $arrFirstUser = $userRepository->findByEmail('tmoxon0@buzzfeed.com');
+        $arrSecondUser = $userRepository->findByEmail('ftrayes1@answers.com');
+
+        //The first User Entity which will be found on database.
+        $firstUser = new User();
+        $firstUser->id = 1;
+        $firstUser->email = 'tmoxon0@buzzfeed.com';
+        $firstUser->firstname = 'Tim';
+        $firstUser->lastname = 'Moxon';
+        $firstUser->username = 'tmoxon0';
+
+        //The second User Entity which will be found on database.
+        $secondUser = new User();
+        $secondUser->id = 2;
+        $secondUser->email = 'ftrayes1@answers.com';
+        $secondUser->firstname = 'Fleurette';
+        $secondUser->lastname = 'Trayes';
+        $secondUser->username = 'ftrayes1';
+
+        //The first and second User Entity should be found on database.
+        $this->assertEquals(1, count($arrFirstUser));
+        $this->assertEquals($firstUser, $arrFirstUser[0]);
+        $this->assertEquals(1, count($arrSecondUser));
+        $this->assertEquals($secondUser, $arrSecondUser[0]);
+
+        //try to find a not available User Entity.
+        $arrNotFoundUser = $userRepository->findByEmail('notavailable@example.com');
+
+        //the User Entity should not be found on database.
+        $this->assertEquals(0, count($arrNotFoundUser));
+        $this->assertEquals([], $arrNotFoundUser);
     }
 
     /**
@@ -68,6 +134,16 @@ class UserRepositoryTest extends DatabaseTestCase
      */
     public function testFindByID()
     {
+        //try to use another DataMapper on UserRepository then UserMapper.
+        $imageMapper = new ImageMapper($this->getConnection()->getConnection());
+        $failedUserRepository = new UserRepository($imageMapper);
+
+        //try to find a User Entity with the wrong DataMapper.
+        $arrUsers = $failedUserRepository->findByID(1);
+
+        //there should be no User Entity.
+        $this->assertEquals(0, count($arrUsers));
+
         //get the UserMapper and UserRepository to get the User Entities.
         $userMapper = new UserMapper($this->getConnection()->getConnection());
         $userRepository = new UserRepository($userMapper);
@@ -93,7 +169,70 @@ class UserRepositoryTest extends DatabaseTestCase
         $secondUser->username = 'ftrayes1';
 
         //The first and second User Entity should be found on database.
+        $this->assertEquals(1, count($arrFirstUser));
         $this->assertEquals($arrFirstUser[0], $firstUser);
+        $this->assertEquals(1, count($arrSecondUser));
         $this->assertEquals($arrSecondUser[0], $secondUser);
+
+        //try to find a not available User Entity.
+        $arrNotFoundUser = $userRepository->findByID(3);
+
+        //the User Entity should not be found on database.
+        $this->assertEquals(0, count($arrNotFoundUser));
+        $this->assertEquals([], $arrNotFoundUser);
+    }
+
+    /**
+     * Method to test the UserRepository:findByUsername method.
+     * @test
+     */
+    public function testFindByUsername()
+    {
+        //try to use another DataMapper on UserRepository then UserMapper.
+        $imageMapper = new ImageMapper($this->getConnection()->getConnection());
+        $failedUserRepository = new UserRepository($imageMapper);
+
+        //try to find a User Entity with the wrong DataMapper.
+        $arrUsers = $failedUserRepository->findByUsername('tmoxon0');
+
+        //there should be no User Entity.
+        $this->assertEquals(0, count($arrUsers));
+
+        //get the UserMapper and UserRepository to get the User Entities.
+        $userMapper = new UserMapper($this->getConnection()->getConnection());
+        $userRepository = new UserRepository($userMapper);
+
+        //get the first and second User Entity from database.
+        $arrFirstUser = $userRepository->findByUsername('tmoxon0');
+        $arrSecondUser = $userRepository->findByUsername('ftrayes1');
+
+        //The first User Entity which will be found on database.
+        $firstUser = new User();
+        $firstUser->id = 1;
+        $firstUser->email = 'tmoxon0@buzzfeed.com';
+        $firstUser->firstname = 'Tim';
+        $firstUser->lastname = 'Moxon';
+        $firstUser->username = 'tmoxon0';
+
+        //The second User Entity which will be found on database.
+        $secondUser = new User();
+        $secondUser->id = 2;
+        $secondUser->email = 'ftrayes1@answers.com';
+        $secondUser->firstname = 'Fleurette';
+        $secondUser->lastname = 'Trayes';
+        $secondUser->username = 'ftrayes1';
+
+        //The User Entity should be found on database.
+        $this->assertEquals(1, count($arrFirstUser));
+        $this->assertEquals($firstUser, $arrFirstUser[0]);
+        $this->assertEquals(1, count($arrSecondUser));
+        $this->assertEquals($secondUser, $arrSecondUser[0]);
+
+        //try to find a not available User Entity.
+        $arrNotFoundUser = $userRepository->findByUsername('notavailable');
+
+        //the User Entity should not be found on database.
+        $this->assertEquals(0, count($arrNotFoundUser));
+        $this->assertEquals([], $arrNotFoundUser);
     }
 }
